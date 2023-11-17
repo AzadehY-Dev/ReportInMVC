1763e07ae17ba6b2d665853304ccfc3d9f3a0fd7
CarlaY <47314411+CarlaY-Dev@users.noreply.github.com>
2023-11-18 03:14:04 +0330

    Sample of Stimulsoft Report in ASP.Net MVC

---
+++ Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Report.Models
+{
+    public class Person
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "please enter {0}")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "please enter {0}")]
+        public string Family { get; set; }
+
+        public string WebSite { get; set; }
+
+    }
+}
